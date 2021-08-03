d61e03b6aca423fd5cdae764b513aa566ab6eeb1
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-08-03 08:51:02 +0300

    add pass

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ app = FastAPI()
 database = {
     "focus": {
         "name": "kostya",
-        "password": "123"
+        "password": "d8fa8488d7ac553529c7f21ae9a9081713f0e89f28316fb9b1706996ab0f8b7f"
     }
 }
 
 # for example. use ENV in future
 SECRET_KEY = "98288a54b00ffaaefe4d66bd71becb050d4db3aadb2590e7b8fbe3f74656604"
+PASSWORD_SALT = "a6e975ceb69c40e1895c7bda2e34d419eae2ed286eb0eca1a6c1509d932be157"
 
 
 def sign_data(data: str) -> str:
@@ -43,6 +44,10 @@ def get_username_from_signed_data(data: str) -> Optional[str]:
     return None
 
 
+def verify_password(password: str, password_hash: str) -> bool:
+    return hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower() == password_hash.lower()
+
+
 @app.get("/")
 def index_page(username: Optional[str] = Cookie(default=None)):
     with open("templates/login.html", "r") as fd:
@@ -65,7 +70,7 @@ def index_page(username: Optional[str] = Cookie(default=None)):
 @app.post("/login")
 def process_login_page(username: str = Form(...), password: str = Form(...)):
     user = database.get(username)
-    if not user or user["password"] != password:
+    if not user or not verify_password(password, user["password"]):
         return Response("Я вас не знаю", media_type="text/html")
 
     response = Response(f"Hello, {username}. (привет)", media_type="text/html")
