84545b18cc0c4a50f177b9ee2e1bff5b6875f80b
julimorozova <yuli.sleptsova@gmail.com>
2021-08-03 17:52:48 +0300

    Add event listener for button

---
+++ templates/app.js
@@ -0,0 +1,34 @@
+console.log('Start!');
+
+const btn = document.querySelector('.form__btn input');
+const userName = document.querySelector('.form__auto #username');
+const userPassword = document.querySelector('.form__auto #password');
+btn.addEventListener('click', (event) => {
+    console.log('event');
+    event.preventDefault();
+
+    console.log(sendRequestAuthorization(userName.value, userPassword.value))
+    userName.value = '';
+    userPassword.value = '';
+
+});
+
+async function sendRequestAuthorization(userName, userPassword) {
+    const url = 'https://api.github.com/repos/julimorozova/twitter/commits';
+
+    const user = {
+        name: userName,
+        password: userPassword
+    }
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+
+        },
+        body: JSON.stringify(user)
+    });
+    console.log(response)
+    let result = await response.json();
+    console.log(result)
+    return result[0].author.login;
+}
