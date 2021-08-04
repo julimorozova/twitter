18a1c28a345234bea9df8ce9da399178945de537
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-08-04 18:53:28 +0300

    add Body

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import Form, Cookie, Response, APIRouter, Body
 
 import base64
@@ -15,16 +17,24 @@ def process_login_page(request_data: dict = Body(...)):
     username = request_data['username']
     password = request_data['password']
 
-    user = data.database.get(username)
-    if not user or not crypto.verify_password(password, user["password"]):
-        return Response("Я вас не знаю", media_type="text/html")
+    return Response(
+        json.dumps({
+            "success": True,
+            "message": "Привет"
+        }),
+        media_type="application/json"
+    )
 
-    page_user = read_file("static/html/userpage.html")
-    response = Response(page_user, media_type="text/html")
-
-    username_signed = base64.b64encode(username.encode()).decode() + "." + crypto.sign_data(username)
-    response.set_cookie(key="username", value=username_signed)
-    return response
+    # user = data.database.get(username)
+    # if not user or not crypto.verify_password(password, user["password"]):
+    #     return Response("Я вас не знаю", media_type="text/html")
+    #
+    # page_user = read_file("static/html/userpage.html")
+    # response = Response(page_user, media_type="text/html")
+    #
+    # username_signed = base64.b64encode(username.encode()).decode() + "." + crypto.sign_data(username)
+    # response.set_cookie(key="username", value=username_signed)
+    # return response
 
 
 
