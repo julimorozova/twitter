2019cf2aba0ddf266674159edc4623f2e731ac9c
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-08-04 19:25:15 +0300

    add Body

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -14,27 +14,39 @@ router = APIRouter()
 
 @router.post("/login")
 def process_login_page(request_data: dict = Body(...)):
-    username = request_data['username']
-    password = request_data['password']
+    try:
+        username = request_data['username']
+        password = request_data['password']
+    except KeyError:
+        return Response(
+            json.dumps({
+                "success": False,
+                "message": "Error when parsing data"
+            }),
+            media_type="application/json"
+        )
 
-    return Response(
-        json.dumps({
-            "success": True,
-            "message": "Привет"
-        }),
-        media_type="application/json"
-    )
+    user = data.database.get(username)
+    if not user or not crypto.verify_password(password, user["password"]):
+        return Response(
+            json.dumps({
+                "success": False,
+                "message": "I don't know you"
+            }),
+            media_type="application/json"
+        )
 
-    # user = data.database.get(username)
-    # if not user or not crypto.verify_password(password, user["password"]):
-    #     return Response("Я вас не знаю", media_type="text/html")
-    #
     # page_user = read_file("static/html/userpage.html")
-    # response = Response(page_user, media_type="text/html")
-    #
+    return Response(
+            json.dumps({
+                "success": True,
+                "message": "I have page",
+                "page": "static/html/userpage.html"
+            }),
+            media_type="application/json"
+        )
+
+
     # username_signed = base64.b64encode(username.encode()).decode() + "." + crypto.sign_data(username)
     # response.set_cookie(key="username", value=username_signed)
-    # return response
-
-
 
