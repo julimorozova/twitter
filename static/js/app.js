4a3cfaca5cae093e94fc39c82f70ff0138c619d0
julimorozova <yuli.sleptsova@gmail.com>
2021-08-05 15:45:33 +0300

    add validation when sending empty fields, put the fetch request in a separate function

--- static/js/app.js
+++ static/js/app.js
@@ -9,9 +9,17 @@ const userPassword = document.querySelector('.form__auto #password');
 btn.addEventListener('click',  async function (event) {
     event.preventDefault();
 
-    const userName = document.querySelector('.form__auto #username');
-    const userPassword = document.querySelector('.form__auto #password');
+    if(validateDate(userName.value, userPassword.value)) {
 
+       let response_json = await sendAuthorizationRequest();
+        hrefPage(response_json);
+
+        userName.value = '';
+        userPassword.value = '';
+    }
+});
+
+async function sendAuthorizationRequest() {
     let response = await fetch("/login", {
         method: 'POST',
         headers: {
@@ -19,26 +27,40 @@ btn.addEventListener('click',  async function (event) {
         },
         body: `{"username": "${userName.value}", "password": "${userPassword.value}"}`,
     });
-    let response_json = await response.json();
-    console.log(response);
-    console.log(response_json);
-    isValidUser(response_json.success);
-    if (response_json.success) {
-        console.log("ПОЛУЧИЛОСЬ");
-        document.location.href = response_json.page;
-
-    }
-    userName.value = '';
-    userPassword.value = '';
-
-});
+    return await response.json();
+}
 
 function isValidUser(success) {
-    let errorUser = `
-        <p style="color: red;">Неверный логин или пароль</p>
-    `
     if (!success) {
-        errorData.innerHTML = errorUser;
+        document.querySelector('#errorData').innerHTML = '*неверный логин или пароль';
+        return false
     }
+    return true;
+}
+
+function validateDate(name, password){
+    if (name.length === 0 || password.length === 0){
+        document.querySelector("#errorData").innerHTML="*заполните все поля ввода";
+        return false;
+    }
+    //Проверим содержит ли значение введенное в поле email символы @ и .
+    /*let at = name.indexOf("@");
+    let dot = name.indexOf(".");
+    //Если поле не содержит эти символы знач email введен не верно
+    if (at < 1 || dot < 1 ) {
+        document.querySelector("#errorData").innerHTML="*email введен не верно";
+        return false;
+    }*/
+    return true;
+
+}
+
+function hrefPage(response_json) {
+    console.log(response_json);
+    if (isValidUser(response_json.success)) {
+        console.log("ПОЛУЧИЛОСЬ");
+        document.location.href = response_json.page;
+    }
+    return document.location.href;
 }
 
